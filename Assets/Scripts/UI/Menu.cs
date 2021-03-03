b23511cd19d88b80198757dfd0a59cecac1954fd
Michael Yamada <roninnarasu@gmail.com>
2021-03-03 15:42:45 +0100

    menu and netowrk stuff WIP

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MenuType { Loading, Title }
+
+public class Menu : MonoBehaviour
+{
+    public MenuType menuType;
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
